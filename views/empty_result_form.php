ec3df33716e2de6a905791484ea0acbff5eea0e7
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-07-29 19:38:28 -0300

    teste

--- views/empty_result_form.php
+++ views/empty_result_form.php
@@ -202,6 +202,12 @@
 
           <p style="color: yellow;">Ao preencher meus dados, concordo em receber comunicações sobre produtos e serviços, conforme a Política de Privacidade.</p>
 
+          <div>
+            <input type="checkbox" name="field[64]" id="check">
+              
+            <label for="check" style="color: green; display: block;">Ao preencher meus dados, concordo em receber comunicações sobre produtos e serviços, conforme a Política de Privacidade.</label>
+          </div>
+
           
 
           <div class="_button-wrapper _full_width">
