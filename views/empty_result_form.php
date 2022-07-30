f14753d479d185e53c0530ee4a4b304fa1c6f686
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-07-29 22:28:14 -0300

    teste

--- views/empty_result_form.php
+++ views/empty_result_form.php
@@ -189,8 +189,8 @@
             </div>
           </div>
 
-          <div>
-            <div class="_field-wrapper d-flex align-items-center">
+          <div class="my-2 d-flex flex-column align-items-center">
+            <div class="d-flex flex-column align-items-center">
               <input type="checkbox" name="field[64]" id="check" required>
                 
               <label for="check">Ao preencher meus dados, concordo em receber comunicações sobre produtos e serviços, conforme a Política de Privacidade.</label>
