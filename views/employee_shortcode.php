7ac52b2d47cbc8b0fa5932edbc0b0ac49d33f66c
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-08-02 23:54:28 -0300

    teste

--- views/employee_shortcode.php
+++ views/employee_shortcode.php
@@ -157,7 +157,7 @@
             div.classList.add('invalid-feedback')
         }
         div.innerText = text;
-        console.log(elem);
+        console.log(elem.parent());
 
         // elem.parentNode.appendChild(div);
     }
