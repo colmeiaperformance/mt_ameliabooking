b4b23141848bb3deb80c54a5264332f057b68c3d
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-08-03 23:43:37 -0300

    teste

--- views/subscription_shortcode.php
+++ views/subscription_shortcode.php
@@ -359,7 +359,7 @@
 
                         console.log("enviou");
 
-                        validateForm(event.attr("id"));
+                        validateForm(event);
                     });
                 });
 
@@ -368,14 +368,14 @@
         }, 500);
     });
 
-    const validateForm = (formId) => {
+    const validateForm = (form) => {
         console.log('validate');
-        console.log(formId);
+        console.log(form);
 
-        let firstName =  jQuery(`#${formId}`).children('.firstName');
-        let lastName =  jQuery(`#${formId}`).children('.lastName');
-        let email =  jQuery(`#${formId}`).children('.email');
-        let phone =  jQuery(`#${formId}`).children('.phone');
+        let firstName =  jQuery(form).children('.firstName');
+        let lastName =  jQuery(form).children('.lastName');
+        let email =  jQuery(form).children('.email');
+        let phone =  jQuery(form).children('.phone');
 
         console.log(firstName);
         console.log(lastName);
