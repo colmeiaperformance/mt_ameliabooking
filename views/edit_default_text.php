2690f31d486685578f55e2aaa17176b6472bab1d
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-08-05 17:18:26 -0300

    teste

--- views/edit_default_text.php
+++ views/edit_default_text.php
@@ -7,29 +7,39 @@ if(!$isAdmin){
     exit;
     die('Você não é admin');
 }
+endelse:
 
-$error = '';
-
-if(isset($_POST['editDefaultText'])){
-    $editDefaultText = $_POST['editDefaultText'];
-    $cleanValue = strip_tags(trim($editDefaultText));
-    $cleanValue = htmlspecialchars($cleanValue);
-    $editDefaultText = str_replace(array("<","WHERE","where",">","=","?"), "", $cleanValue);
+if(isset($_GET['defaultText'])){
+    if(urldecode($_GET['defaultText']) == 'FDadfGHKALD'){
+        echo json_encode((object) ["defaultText" => get_option("mt_defaultText")]);
+    }else{
+        echo json_encode((object) []);
+    }
+} else { 
     
-    function hasError($editDefaultText){
-        $error = '';
+    $error = '';
 
-        if(strlen($editDefaultText) > 400){
-            $error = "O texto não pode conter mais de 400 caracteres.";
-        }
+    if(isset($_POST['editDefaultText'])){
+        $editDefaultText = $_POST['editDefaultText'];
+        $cleanValue = strip_tags(trim($editDefaultText));
+        $cleanValue = htmlspecialchars($cleanValue);
+        $editDefaultText = str_replace(array("<","WHERE","where",">","=","?"), "", $cleanValue);
         
-        return $error;
+        function hasError($editDefaultText){
+            $error = '';
+
+            if(strlen($editDefaultText) > 400){
+                $error = "O texto não pode conter mais de 400 caracteres.";
+            }
+            
+            return $error;
+        }
+
+        if(!($error = hasError($editDefaultText))){
+            update_option("mt_defaultText", $editDefaultText);
+        }
     }
 
-    if(!($error = hasError($editDefaultText))){
-        update_option("mt_defaultText", $editDefaultText);
-    }
-}
 ?>
 
 <div class="container-md">
@@ -38,12 +48,12 @@ if(isset($_POST['editDefaultText'])){
             <label for="editDefaultText" class="form-label">Digite seu texto padrão</label>
             <textarea name="editDefaultText" class="form-control" id="editDefaultText" rows="6"><?= $error != '' ? $editDefaultText : '' ?></textarea>
             <?= $error != '' ? "<div class='invalid-feedback' style='display: block;'>$error</div>" : '' ?>
-            <?= $error == '' ? "<div class='valid-feedback' style='display: block;'>Texto salvo com sucesso!</div>" : '' ?>
+            <?= $error == '' && isset($_POST['editDefaultText']) ? "<div class='valid-feedback' style='display: block;'>Texto salvo com sucesso!</div>" : '' ?>
         </div>
 
         <button type="submit" class="btn btn-primary">Salvar</button>
     </form>
-    <?= $error == '' ? '<a href="'.site_url().'" class="btn btn-primary btn-lg">Página inicial</a>' : '' ?>
+    <?= $error == '' && isset($_POST['editDefaultText']) ? '<a href="'.site_url().'" class="btn btn-primary btn-lg">Página inicial</a>' : '' ?>
 </div>
 
 <script>
@@ -63,3 +73,5 @@ if(isset($_POST['editDefaultText'])){
         resize: none;
     }
 </style>
+
+<?php } ?>
