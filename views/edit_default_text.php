22c83817ecf24128b9dd7445ebefe51d7705a840
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-08-05 02:31:25 -0300

    teste

--- views/edit_default_text.php
+++ views/edit_default_text.php
@@ -1,10 +1,36 @@
+<?php
+
+echo $_POST['editDefaultText'];
+
+
+echo "_________________________";
+
+var_dump($_SESSION);
+
+
+?>
+
 <div class="container-md">
-    <form>
+    <form id="formEditDefaultText" method="POST" action="#">
         <div class="mb-3">
             <label for="editDefaultText" class="form-label">Digite seu texto padrão</label>
-            <textarea class="form-control" id="editDefaultText" rows="6"></textarea>
+            <textarea name="editDefaultText" class="form-control" id="editDefaultText" rows="6"></textarea>
         </div>
 
         <button type="submit" class="btn btn-primary">Salvar</button>
     </form>
 </div>
+
+<style>
+    #formEditDefaultText button {
+        background-color: #F29F05;
+    }
+
+    #formEditDefaultText button:hover {
+        background-color: #FFC536;
+    }
+
+    #formEditDefaultText textarea {
+        resize: none;
+    }
+</style>
