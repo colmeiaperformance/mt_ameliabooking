0951b47d3063a4b88790fb5638d71e71173cd6fb
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-08-09 21:41:37 -0300

    teste

--- mt_ameliabooking.php
+++ mt_ameliabooking.php
@@ -16,18 +16,6 @@
 function add_plugins_and_scripts(){
    wp_register_style('add-mt-ameleia-css', plugin_dir_url(__FILE__).'views/styles/main.css?v=3213', '', '', 'screen');
 
-
-   // wp_register_style('add-mt-amelia-intl-tel-input-css', plugin_dir_url(__FILE__).'views/js/util/intl-tel-input/css/intlTelInput.css', '', '', 'screen');
-
-   // wp_register_style('add-mt-amelia-intl-tel-input-style-css', plugin_dir_url(__FILE__).'views/js/util/intl-tel-input/css/style.css', '', '', 'screen');
-
-   // wp_register_script('add-mt-amelia-intl-tel-input-js', plugin_dir_url(__FILE__).'views/js/util/intl-tel-input/js/intlTelInput.js', '', null, '');
-   
-   // wp_register_script('add-mt-amelia-intl-tel-input-utils-js', plugin_dir_url(__FILE__).'views/js/util/intl-tel-input/js/utils.js', '', null, '');
-   
-   // wp_register_script('add-mt-amelia-intl-tel-input-script-js', plugin_dir_url(__FILE__).'views/js/util/intl-tel-input/js/script.js', '', null, '');
-
-
    wp_register_script('add-mt-amelia-axios', 'https://unpkg.com/axios/dist/axios.min.js', '', null, '');
    wp_register_script('add-mt-amelia-moment', 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.3/moment.min.js', '', null, '');
    wp_register_script('add-mt-amelia-mask', plugin_dir_url(__FILE__).'views/js/util/jquery.mask.min.js', '', null, '');
@@ -60,14 +48,7 @@ function add_plugins_and_scripts(){
    }
    wp_enqueue_style('add-mt-ameleia-css');
    
-   // wp_enqueue_style('add-mt-amelia-intl-tel-input-css');
-   // wp_enqueue_style('add-mt-amelia-intl-tel-input-style-css');
-   
    wp_enqueue_style('');
-
-   // wp_enqueue_script('add-mt-amelia-intl-tel-input-js');
-   // wp_enqueue_script('add-mt-amelia-intl-tel-input-utils-js');
-   // wp_enqueue_script('add-mt-amelia-intl-tel-input-script-js');
    
    
    wp_enqueue_script('add-mt-amelia-axios');
