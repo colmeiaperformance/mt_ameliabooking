cd7472f993e8ccd1ee0a9e107d05bdbe0d1f37ec
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-08-10 00:55:32 -0300

    teste

--- views/js/views/EventItem.js
+++ views/js/views/EventItem.js
@@ -88,7 +88,7 @@ class EventItem extends View{
 									</button>
 								</div>
 							</div>
-							<form id="formEvt${e.id}" class="needs-validation form-events">
+							<form id="formEvt${e.id}" class="needs-validation" method="post">
 							<div class="mt_event_details_subscriptions" id="mt_event_details_subscriptions_${key}">
 								<div class="mt_row">
 									<div class="mt_column">
