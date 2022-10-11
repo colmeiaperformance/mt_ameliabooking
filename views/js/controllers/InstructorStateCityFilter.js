4d86c5eed29aa63687f419577be401808389248e
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-10-11 20:02:26 -0300

    teste

--- views/js/controllers/InstructorStateCityFilter.js
+++ views/js/controllers/InstructorStateCityFilter.js
@@ -5,12 +5,22 @@ class InstructorStateCityFilter {
         this._listStateCity = this.getStateAndCity(employeees);
     }
 
-    getStateAndCity = async(employeees) => {
+    getStateAndCity =  employeees => {
         // console.log("user infos");
         // console.log(userInfos);
+        let entities_consult = axios.get(`${this._ajaxUrl}/?action=wpamelia_api&call=/entities&types[]=employees&types[]=locations`);
+
+        console.log("entities consult");
+        console.log(entities_consult);
 
         console.log("employeees");
         console.log(employeees);
+
+        employeees.forEach(element => {
+            console.log("Element");
+            console.log(element);
+        });
+        
 
         return [];
         
