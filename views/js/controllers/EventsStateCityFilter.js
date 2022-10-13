033c2b5309f95d49944969fba86667f39f5f07e0
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-10-13 00:00:25 -0300

    teste

--- views/js/controllers/EventsStateCityFilter.js
+++ views/js/controllers/EventsStateCityFilter.js
@@ -6,72 +6,72 @@ class EventsStateCityFilter {
         this._listStateCity = [];
         this.states = [];
         this.cities = [];
-        this.getStateAndCity();
+        // this.getStateAndCity();
     }
 
     getStateAndCity =  async() => {
         let locList = [];
         let listStateCity = [];
 
-        // console.log("Events List");
-        // console.log(this._eventsList);
+        console.log("Events List");
+        console.log(this._eventsList);
 
-        // let states = [];
-        // let cities = [];
+        let states = [];
+        let cities = [];
 
-        // this._eventsList.forEach(element => {
-        //     console.log(element._local);
-        //     locList.push(element._local);
-        // });
+        this._eventsList.forEach(element => {
+            console.log(element._local);
+            locList.push(element._local);
+        });
 
-        // console.log("Loc list");
-        // console.log(locList);
+        console.log("Loc list");
+        console.log(locList);
 
-        // locList.forEach(element => {
-        //     let temp = element.split("-");
+        locList.forEach(element => {
+            let temp = element.split("-");
             
-        //     temp[0] = temp[0].trim();
-        //     temp[1] = temp[1].trim();
+            temp[0] = temp[0].trim();
+            temp[1] = temp[1].trim();
 
-        //     if(temp[0].length == 2) {
-        //         temp[0] = temp[0].toUpperCase();
-        //         temp[1] = temp[1];
+            if(temp[0].length == 2) {
+                temp[0] = temp[0].toUpperCase();
+                temp[1] = temp[1];
 
-        //         if(states.indexOf(temp[0]) == -1){
-        //             states.push(temp[0]);
+                if(states.indexOf(temp[0]) == -1){
+                    states.push(temp[0]);
 
-        //             let index = states.indexOf(temp[0]);
+                    let index = states.indexOf(temp[0]);
 
-        //             cities[index] = [temp[1]];
-        //         }else{
-        //             let index = states.indexOf(temp[0]);
+                    cities[index] = [temp[1]];
+                }else{
+                    let index = states.indexOf(temp[0]);
 
-        //             if(cities[index] && cities[index].indexOf(temp[1]) == -1){
-        //                 cities[index].push(temp[1]);
-        //             }
-        //         }
-        //     }else{
-        //         temp[1] = temp[1].toUpperCase();
-        //         temp[0] = temp[0];
+                    if(cities[index] && cities[index].indexOf(temp[1]) == -1){
+                        cities[index].push(temp[1]);
+                    }
+                }
+            }else{
+                temp[1] = temp[1].toUpperCase();
+                temp[0] = temp[0];
 
-        //         if(states.indexOf(temp[1]) == -1){
-        //             states.push(temp[1]);
-        //             let index = states.indexOf(temp[1]);
+                if(states.indexOf(temp[1]) == -1){
+                    states.push(temp[1]);
+                    let index = states.indexOf(temp[1]);
 
 
-        //             cities[index] = [temp[0]];
-        //         }else{
-        //             let index = states.indexOf(temp[1]);
+                    cities[index] = [temp[0]];
+                }else{
+                    let index = states.indexOf(temp[1]);
 
-        //             if(cities[index] && cities[index].indexOf(temp[0]) == -1){
-        //                 cities[index].push(temp[0]);
-        //             }
-        //         }
-        //     }
+                    if(cities[index] && cities[index].indexOf(temp[0]) == -1){
+                        cities[index].push(temp[0]);
+                    }
+                }
+            }
 
-        // });
-        // this.states = states;
-        // this.cities = cities;         
+        });
+        this.states = states;
+        this.cities = cities;         
     }
 
 }
