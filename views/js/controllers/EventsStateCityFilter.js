dc90af90d8f2b68cb8709cb633a93c344a051f89
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-10-12 23:34:15 -0300

    [Update] Estados e cidades onde tem eventos

--- views/js/controllers/EventsStateCityFilter.js
+++ views/js/controllers/EventsStateCityFilter.js
@@ -27,51 +27,51 @@ class EventsStateCityFilter {
         console.log("Loc list");
         console.log(locList);
 
-        // locList.forEach(element => {
-        //     let temp = element.split("-");
+        locList.forEach(element => {
+            let temp = element.split("-");
             
-        //     temp[0] = temp[0].trim();
-        //     temp[1] = temp[1].trim();
+            temp[0] = temp[0].trim();
+            temp[1] = temp[1].trim();
 
-        //     if(temp[0].length == 2) {
-        //         temp[0] = temp[0].toUpperCase();
-        //         temp[1] = temp[1];
+            if(temp[0].length == 2) {
+                temp[0] = temp[0].toUpperCase();
+                temp[1] = temp[1];
 
-        //         if(states.indexOf(temp[0]) == -1){
-        //             states.push(temp[0]);
+                if(states.indexOf(temp[0]) == -1){
+                    states.push(temp[0]);
 
-        //             let index = states.indexOf(temp[0]);
+                    let index = states.indexOf(temp[0]);
 
-        //             cities[index] = [temp[1]];
-        //         }else{
-        //             let index = states.indexOf(temp[0]);
+                    cities[index] = [temp[1]];
+                }else{
+                    let index = states.indexOf(temp[0]);
 
-        //             if(cities[index] && cities[index].indexOf(temp[1]) == -1){
-        //                 cities[index].push(temp[1]);
-        //             }
-        //         }
-        //     }else{
-        //         temp[1] = temp[1].toUpperCase();
-        //         temp[0] = temp[0];
+                    if(cities[index] && cities[index].indexOf(temp[1]) == -1){
+                        cities[index].push(temp[1]);
+                    }
+                }
+            }else{
+                temp[1] = temp[1].toUpperCase();
+                temp[0] = temp[0];
 
-        //         if(states.indexOf(temp[1]) == -1){
-        //             states.push(temp[1]);
-        //             let index = states.indexOf(temp[1]);
+                if(states.indexOf(temp[1]) == -1){
+                    states.push(temp[1]);
+                    let index = states.indexOf(temp[1]);
 
 
-        //             cities[index] = [temp[0]];
-        //         }else{
-        //             let index = states.indexOf(temp[1]);
+                    cities[index] = [temp[0]];
+                }else{
+                    let index = states.indexOf(temp[1]);
 
-        //             if(cities[index] && cities[index].indexOf(temp[0]) == -1){
-        //                 cities[index].push(temp[0]);
-        //             }
-        //         }
-        //     }
+                    if(cities[index] && cities[index].indexOf(temp[0]) == -1){
+                        cities[index].push(temp[0]);
+                    }
+                }
+            }
 
-        // });
-        // this.states = states;
-        // this.cities = cities;         
+        });
+        this.states = states;
+        this.cities = cities;         
     }
 
 }
