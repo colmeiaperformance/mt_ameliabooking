270dcf40fe11330b8f2ba1c8e1cf933fa542ddde
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-10-19 20:19:50 -0300

    teste

--- views/js/controllers/InstructorStateCityFilter.js
+++ views/js/controllers/InstructorStateCityFilter.js
@@ -7,6 +7,7 @@ class InstructorStateCityFilter {
         this._listStateCity = [];
         this.states = [];
         this.cities = [];
+        this.getStateAndCity();
     }
 
     getStateAndCity =  async() => {
