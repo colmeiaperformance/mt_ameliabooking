c4d2423e3b682fc7178ae1b22e2919fe27dd59aa
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-10-27 02:54:20 -0300

    teste

--- views/js/views/FilterFields.js
+++ views/js/views/FilterFields.js
@@ -33,12 +33,12 @@ class FilterFields extends View{
         </div>
       </div>
       <div class="mt_row justify-content-start">
-        <div class="mt_filter input">
+        <div class="mt_filter col-sm-3">
           <input type="text" id="districtFilter" class="form-control" placeholder="Bairro" value="${model.district}" onKeyUp="filterByDistrict(value)" class="form-control" style="display: none;>
         </div>
       </div>
       <div class="ms-auto me-0 d-flex justify-content-center justify-content-lg-end flex-nowrap container-fluid">
-        <div class="mt_filter">
+        <div>
           <button id="removeFilterButton" onclick="removeFilters()" class="btn btn-remove">
           Remover Filtros
           </button>
