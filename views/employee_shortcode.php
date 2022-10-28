5752f4f97edcb2c7533f09b017fe8bc42e0af724
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-10-28 14:44:41 -0300

    [Fix] Correção horário nos eventos da página single do instrutor

--- views/employee_shortcode.php
+++ views/employee_shortcode.php
@@ -742,14 +742,13 @@
 
             if(events.length > 0) {                
                 events.map((e,key) => {
-                    // let baseurl 
                     console.log('baseurl');
                     console.log(urlbase);
                     let startDate = moment(e.periods[0].periodStart);
                     let endDate = moment(e.periods[0].periodEnd);
 
-                    // startDate.subtract(3, 'hours');
-                    // endDate.subtract(3, 'hours');
+                    startDate.subtract(3, 'hours');
+                    endDate.subtract(3, 'hours');
 
                     let weekday = (moment(e.periods[0].periodStart)).format('dddd');
 
@@ -782,35 +781,7 @@
                             weekday = 'SAB';
                             break;
                     }
-                    // '${
-                    // 					e.customFields ?
-                    // 					`
-                    // 						<div class="mt_row">
-                    // 							${
-                    // 								e.customFields.map(e => {
-                    // 									return(`
-                    // 										<div class="mt_column" style="padding:0px 10px">
-                    // 											<label> ${e.label} </label>
-                    // 											<div class="mt_checkbox_container">
-                    // 												${e.options.map(opt => {
-                    // 													return(`
-                    // 														<div class="item">
-                    // 															<input id="contactAceite" type="checkbox" onChange="changeCheckBoxOque(this, ${opt.customFieldId})" value="${opt.label}"  ${opt.required ? 'required' : ''} name="customField${opt.customFieldId}[]" >
-                    // 															<label for="origin">${opt.label}</label>
-                    // 														</div>
-                    // 													`)	
-                    // 												}).join('')}
-                    // 											</div>
-                    // 										</div>
-                    // 									`)
-                    // 								}).join('')
-                    // 							}
-                    // 						</div>
-                    // 					` 
-                    // 					:
-                    // 					``
-                    // 				}'
-
+    
                     const month_labels = ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'];
                     const month_names = ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro']
                     let startDateStr = `${startDate.format('D') } de ${month_names[startDate.month()]} de ${startDate.format('YYYY')}`;
