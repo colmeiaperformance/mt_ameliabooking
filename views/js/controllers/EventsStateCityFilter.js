b53ed834697a9ac330e3fb5a44ca92dd2d71ff16
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-11-01 04:29:22 -0300

    [Fix] Correção de cidades sem estados

--- views/js/controllers/EventsStateCityFilter.js
+++ views/js/controllers/EventsStateCityFilter.js
@@ -30,46 +30,49 @@ class EventsStateCityFilter {
         console.log(locList);
 
         locList.forEach(element => {
-            if(element){
-                let temp = element.split("-");
+            if(element && element != ""){
+                if(element.includes("-")){
+                    let temp = element.split("-");
 
-                console.log(temp);
+                    if((temp[0] && temp[0] != "") && (temp[1] && temp[1] != "")){
             
-                temp[0] = temp[0].trim();
-                temp[1] = temp[1].trim();
+                        temp[0] = temp[0].trim();
+                        temp[1] = temp[1].trim();
 
-                if(temp[0].length == 2) {
-                    temp[0] = temp[0].toUpperCase();
-                    temp[1] = temp[1];
+                        if(temp[0].length == 2) {
+                            temp[0] = temp[0].toUpperCase();
+                            temp[1] = temp[1];
 
-                    if(states.indexOf(temp[0]) == -1){
-                        states.push(temp[0]);
+                            if(states.indexOf(temp[0]) == -1){
+                                states.push(temp[0]);
 
-                        let index = states.indexOf(temp[0]);
+                                let index = states.indexOf(temp[0]);
 
-                        cities[index] = [temp[1]];
-                    }else{
-                        let index = states.indexOf(temp[0]);
+                                cities[index] = [temp[1]];
+                            }else{
+                                let index = states.indexOf(temp[0]);
 
-                        if(cities[index] && cities[index].indexOf(temp[1]) == -1){
-                            cities[index].push(temp[1]);
-                        }
-                    }
-                }else{
-                    temp[1] = temp[1].toUpperCase();
-                    temp[0] = temp[0];
+                                if(cities[index] && cities[index].indexOf(temp[1]) == -1){
+                                    cities[index].push(temp[1]);
+                                }
+                            }
+                        }else{
+                            temp[1] = temp[1].toUpperCase();
+                            temp[0] = temp[0];
 
-                    if(states.indexOf(temp[1]) == -1){
-                        states.push(temp[1]);
-                        let index = states.indexOf(temp[1]);
+                            if(states.indexOf(temp[1]) == -1){
+                                states.push(temp[1]);
+                                let index = states.indexOf(temp[1]);
 
 
-                        cities[index] = [temp[0]];
-                    }else{
-                        let index = states.indexOf(temp[1]);
+                                cities[index] = [temp[0]];
+                            }else{
+                                let index = states.indexOf(temp[1]);
 
-                        if(cities[index] && cities[index].indexOf(temp[0]) == -1){
-                            cities[index].push(temp[0]);
+                                if(cities[index] && cities[index].indexOf(temp[0]) == -1){
+                                    cities[index].push(temp[0]);
+                                }
+                            }
                         }
                     }
                 }
