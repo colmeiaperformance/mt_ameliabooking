323b43dfc7e83de69222acf697372ae5a6f6b0f9
Fabiano Mendes <fabianomendesoficial@gmail.com>
2022-10-31 23:53:39 -0300

    [Update] Atualização na versão do plugin

--- mt_ameliabooking.php
+++ mt_ameliabooking.php
@@ -3,7 +3,7 @@
    Plugin Name: Amelia Booking for Meditação Transcendental
    Plugin URI: https://github.com/jeffersonalopes
    description: Plugin para adição de funcionalidades para o plugin Amelia Booking - site Meditação Transcedental.
-   Version: 1.0
+   Version: 2.0
    Author: Jefferson Augusto Lopes
    Author URI: https://github.com/jeffersonalopes
    License: GPL2
