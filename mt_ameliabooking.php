9ad2a9440613ec1f826141419d1277b5394d391a
Becky Marques <becky.marques@colmeiaperformance.com.br>
2022-11-09 20:02:26 -0300

    Register axios and moment js

--- mt_ameliabooking.php
+++ mt_ameliabooking.php
@@ -16,8 +16,8 @@
 function add_plugins_and_scripts(){
    wp_register_style('add-mt-ameleia-css', plugin_dir_url(__FILE__).'views/styles/main.css?v=3213', '', '', 'screen');
 
-   wp_register_script('add-mt-amelia-axios', 'https://unpkg.com/axios/dist/axios.min.js', '', null, '');
-   wp_register_script('add-mt-amelia-moment', 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.3/moment.min.js', '', null, '');
+   wp_register_script('add-mt-amelia-axios', plugin_dir_url(__FILE__).'views/js/util/axios.min.js', '', null, '');
+   wp_register_script('add-mt-amelia-moment', plugin_dir_url(__FILE__).'views/js/util/moment.min.js', '', null, '');
    wp_register_script('add-mt-amelia-mask', plugin_dir_url(__FILE__).'views/js/util/jquery.mask.min.js', '', null, '');
    wp_register_script('add-mt-amelia-phone', plugin_dir_url(__FILE__).'views/js/util/phone.mask.js', '', null, '');
    wp_register_script('add-mt-amelia-form-validation', plugin_dir_url(__FILE__).'views/js/util/form-validation.js', '', null, '');
