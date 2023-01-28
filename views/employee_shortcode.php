3ddc03ec8200415d5059dd41f4400f32e9062cf7
Fabiano Mendes <fabianomendesoficial@gmail.com>
2023-01-28 03:40:57 -0300

    teste

--- views/employee_shortcode.php
+++ views/employee_shortcode.php
@@ -438,4 +438,25 @@
     }
 
     render();
+
+    (() => {
+  'use strict'
+
+  jQuery('.needs-validation').ready(function() {
+    const forms = document.querySelectorAll('.needs-validation')
+
+    forms.forEach(form => {
+      form.addEventListener('submit', event => {
+        event.preventDefault();
+        
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+  });
+})()
 </script>
